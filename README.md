ddd1e8dbfe47ecaab433ecfc85885cf1d75e4cff
Doug Pfeffer <dougpfeffer2013@gmail.com>
2020-09-23 17:11:07 -0700

    Added the Blog page and sorted CSS

--- README.md
+++ README.md
@@ -24,3 +24,5 @@ September 13, 2020 - Updated projects to include the latest Code Fellows Project
 September 19, 2020 - Swapped out a static photo for my 201 project, with a Wheel of Fortune GIF similar to what is found on the actual game site. Also adjusted some styling, and adjusted the font size of the footer.
 
 September 22, 2020 - Added a Biography page, and a link to a future Blog page
+
+September 23, 2020 - Sorted my CSS files, and created the blog page.
--- app.js
+++ app.js
@@ -1,106 +1,126 @@
-function sayHello() {
-    console.log('Hello World!');
+'use strict';
+
+window.addEventListener('scroll',
+    moveScrollIndicator);
+
+const scrollIndicatorElt =
+    document.getElementById('scrollIndicator');
+
+const maxHeight =
+    window.document.body.scrollHeight
+    - window.innerHeight;
+
+function moveScrollIndicator(e) {
+    const percentage =
+        ((window.scrollY) / maxHeight) * 100;
+
+    scrollIndicatorElt.style.width
+        = percentage + '%';
 }
 
-sayHello();
+// function sayHello() {
+//     console.log('Hello World!');
+// }
 
-var sayHellowCookie = function () {
-    console.log('Hello Cookie')
-}
+// sayHello();
 
-function sayHelloUser(name) {
-    console.log('Hello ' + name);
-}
+// var sayHellowCookie = function () {
+//     console.log('Hello Cookie')
+// }
 
-//First Function//
+// function sayHelloUser(name) {
+//     console.log('Hello ' + name);
+// }
 
-function askName() {
-    var name = prompt('What is your name');
-    var message = 'Hello, ' + name;
+// //First Function//
 
-    return document.write('<h3>' + message + '</h3>');
+// function askName() {
+//     var name = prompt('What is your name');
+//     var message = 'Hello, ' + name;
 
-}
+//     return document.write('<h3>' + message + '</h3>');
 
-//Second Function//
+// }
 
-function askPreference() {
-    var preference = prompt('Are you a Casual Rider or a Racer?');
-    var response;
+// //Second Function//
 
-    while (preference !== 'Casual Rider' && preference !== 'Racer'){
-        preference = prompt('Please answer exactly! Are you a Casual Rider or a Racer?')
-    }
+// function askPreference() {
+//     var preference = prompt('Are you a Casual Rider or a Racer?');
+//     var response;
 
-    if (preference === 'Casual Rider') {
-        response = 'Great, Bikes are Cool!';
-    } else if (preference === 'Racer') {
-        response = 'Yes! Racing is Awesome';
-    } else {
-        response = 'Fine, don\'t answer!';
-    }
+//     while (preference !== 'Casual Rider' && preference !== 'Racer'){
+//         preference = prompt('Please answer exactly! Are you a Casual Rider or a Racer?')
+//     }
 
-    return document.write('<h3>' + response + '</h3>');
+//     if (preference === 'Casual Rider') {
+//         response = 'Great, Bikes are Cool!';
+//     } else if (preference === 'Racer') {
+//         response = 'Yes! Racing is Awesome';
+//     } else {
+//         response = 'Fine, don\'t answer!';
+//     }
 
-}
+//     return document.write('<h3>' + response + '</h3>');
 
-function getStarCount(){
-    var starCount = prompt('How many stars would you rate your last ride? 1 to 5.  ');
-    
-    while (isNaN(starCount) || starCount === ''){
-      starCount = prompt('PLEASE enter a number!  How would you rate that ride?');
-    }
-    return starCount
-  }
-  
-  //example conditional statement - evaluates to false becasue one false ejects from conditional
-  // ((500 * 2 === 1000) && (false && true) && true)
-  
-  function showOrder(){
-    var result = '';
-    var itemType = askPreference();
-    var total = getCount();
-  
-    for (var i = 0; i < total; i++){
-      result = result + '<p>' + itemType + '</p>';
-    }
-  
-    return document.write(result);
-  }
-  
-  function showRating(){
-    var result = '';
-    var starCount = getStarCount();
-  
-    for (var i = 0; i < starCount; i++){
-      result = result + '<p><img src="images/star.png"></p>';
-    }
-  
-    return document.write(result);
-  }
+// }
 
-//Third Function//
+// function getStarCount(){
+//     var starCount = prompt('How many stars would you rate your last ride? 1 to 5.  ');
 
-function addContent() {
-    var today = new Date();
-    var hourNow = today.getHours();
+//     while (isNaN(starCount) || starCount === ''){
+//       starCount = prompt('PLEASE enter a number!  How would you rate that ride?');
+//     }
+//     return starCount
+//   }
+
+//   //example conditional statement - evaluates to false becasue one false ejects from conditional
+//   // ((500 * 2 === 1000) && (false && true) && true)
+
+//   function showOrder(){
+//     var result = '';
+//     var itemType = askPreference();
+//     var total = getCount();
+
+//     for (var i = 0; i < total; i++){
+//       result = result + '<p>' + itemType + '</p>';
+//     }
+
+//     return document.write(result);
+//   }
+
+//   function showRating(){
+//     var result = '';
+//     var starCount = getStarCount();
+
+//     for (var i = 0; i < starCount; i++){
+//       result = result + '<p><img src="images/star.png"></p>';
+//     }
+
+//     return document.write(result);
+//   }
+
+// //Third Function//
+
+// function addContent() {
+//     var today = new Date();
+//     var hourNow = today.getHours();
 
 
-    if (hourNow > 18) {
-        greeting = 'Good evening!';
-    } else if (hourNow > 12) {
-        greeting = 'Good afternoon!';
-    } else if (hourNow > 0) {
-        greeting = 'Good morning!';
-    } else {
-        greeting = 'Welcome';
-    }
+//     if (hourNow > 18) {
+//         greeting = 'Good evening!';
+//     } else if (hourNow > 12) {
+//         greeting = 'Good afternoon!';
+//     } else if (hourNow > 0) {
+//         greeting = 'Good morning!';
+//     } else {
+//         greeting = 'Welcome';
+//     }
 
-    return document.write('<h3>' + greeting + '</h3>');
-}
+//     return document.write('<h3>' + greeting + '</h3>');
+// }
 
-function buildBike(bikeType, barType, tireType ) {
-    var message = 'I want a ' + bikeType + 'bike with ' + barType + 'bars ' + tireType + 'tires.'
-    return message;
-}
+// function buildBike(bikeType, barType, tireType ) {
+//     var message = 'I want a ' + bikeType + 'bike with ' + barType + 'bars ' + tireType + 'tires.'
+//     return message;
+// }
 
